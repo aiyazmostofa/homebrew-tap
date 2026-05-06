dd3f2cadd5c36af43b372470affe07c63d76346f
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-05-05 21:34:27 -0500

    Fixed tree sitter support

--- Formula/emacs.rb
+++ Formula/emacs.rb
@@ -5,11 +5,21 @@ class Emacs < Formula
   sha256 "b3f36f18a6dd2715713370166257de2fae01f9d38cfe878ced9b1e6ded5befd9"
   license "GPL-3.0-or-later"
 
+  depends_on "pkgconf" => :build
+  depends_on "gcc" => :build
+  depends_on "coreutils" => :build
+
   depends_on "gnutls"
   depends_on "gtk+3"
   depends_on "tree-sitter"
 
   def install
+    # Hack to make this build on immutable distros (where /home -> /var/home)
+    ENV["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.realpath
+
+    # Tree sitter patches
+    inreplace "src/treesit.c", "ts_language_version", "ts_language_abi_version"
+
     system "./configure", "--with-pgtk", "--with-tree-sitter", *std_configure_args
     system "make", "install"
   end

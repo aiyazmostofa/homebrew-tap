1b4e1397816a54f79d80a6eb3b052cbebb5d3086
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-05-05 20:55:20 -0500

    Setting up for GH CI

--- Formula/emacs.rb
+++ Formula/emacs.rb
@@ -1,12 +1,12 @@
 class Emacs < Formula
-  desc "Emacs"
+  desc "An extensible, customizable, free/libre text editor"
   homepage "https://www.gnu.org/software/emacs"
   url "https://ftpmirror.gnu.org/emacs/emacs-30.2.tar.xz"
   sha256 "b3f36f18a6dd2715713370166257de2fae01f9d38cfe878ced9b1e6ded5befd9"
   license "GPL-3.0-or-later"
 
-  depends_on "gtk+3"
   depends_on "gnutls"
+  depends_on "gtk+3"
 
   def install
     system "./configure", "--with-pgtk", *std_configure_args

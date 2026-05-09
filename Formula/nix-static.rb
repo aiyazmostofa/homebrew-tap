38ae8a43da0e8c8f1a2f1c7f7ac6cd93ac652d2c
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-05-09 09:54:40 -0500

    All hail brew style

--- Formula/nix-static.rb
+++ Formula/nix-static.rb
@@ -1,5 +1,5 @@
 class NixStatic < Formula
-  desc "The purely functional package manager"
+  desc "Purely functional package manager"
   homepage "https://nixos.org"
   url "https://github.com/aiyazmostofa/nixStatic/releases/download/2.34.7/nixStatic-2.34.7.tar.gz"
   sha256 "7387d2b12f0e725a4e00d4e2adb28325fedb6ec69c6728efecd5f3b0d7e55cd8"

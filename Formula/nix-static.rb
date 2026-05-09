72e4d71cca4250f2b40e03e55663334a27eb3212
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-05-09 09:50:51 -0500

    Nix

---
+++ Formula/nix-static.rb
@@ -0,0 +1,12 @@
+class NixStatic < Formula
+  desc "The purely functional package manager"
+  homepage "https://nixos.org"
+  url "https://github.com/aiyazmostofa/nixStatic/releases/download/2.34.7/nixStatic-2.34.7.tar.gz"
+  sha256 "7387d2b12f0e725a4e00d4e2adb28325fedb6ec69c6728efecd5f3b0d7e55cd8"
+  license "LGPL-2.1-only"
+
+  def install
+    bin.install Dir["bin/*"]
+    share.install Dir["share/*"]
+  end
+end

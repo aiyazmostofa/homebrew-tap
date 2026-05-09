052a8299f380f49390c36569d3f5e2e23d9214b4
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-05-09 11:11:21 -0500

    changed nmae to nix

--- Formula/nix.rb
+++ Formula/nix.rb
@@ -1,7 +1,7 @@
-class NixStatic < Formula
+class Nix < Formula
   desc "Purely functional package manager"
   homepage "https://nixos.org"
-  url "https://github.com/aiyazmostofa/nixStatic/releases/download/2.34.7/nixStatic-2.34.7.tar.gz"
+  url "https://github.com/aiyazmostofa/nix/releases/download/2.34.7/nix-2.34.7.tar.gz"
   sha256 "7387d2b12f0e725a4e00d4e2adb28325fedb6ec69c6728efecd5f3b0d7e55cd8"
   license "LGPL-2.1-only"
 
